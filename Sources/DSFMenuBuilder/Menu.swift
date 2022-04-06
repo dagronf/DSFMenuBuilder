2c7c135277dbc66a2b5285704b3b404117440960
Darren Ford <dford_au-reg@yahoo.com>
2022-04-06 14:32:22 +1000

    Updated formatting

--- Sources/DSFMenuBuilder/Menu.swift
+++ Sources/DSFMenuBuilder/Menu.swift
@@ -1,13 +1,40 @@
+//
+//  Menu.swift
+//  Copyright © 2022 Darren Ford. All rights reserved.
+//
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+//
+
 import AppKit
 
+/// A representation of a menu
 public class Menu {
 	var menuItems: [AnyMenuItem] = []
 
-	/// A lazy NSMenu representation of this menu
-	public lazy var menu: NSMenu = {
+	/// Generate an NSMenu representation of the Menu structure
+	public var menu: NSMenu {
 		return self.build()
-	}()
+	}
 
+	/// Create a Menu object using the specified builder
 	public init(@MenuBuilder builder: () -> [AnyMenuItem]) {
 		self.menuItems = builder()
 	}
@@ -22,6 +49,7 @@ public class Menu {
 
 // MARK: - Builders
 
+/// A menu builder object
 @resultBuilder
 public struct MenuBuilder {
 	public static func buildBlock(_ components: AnyMenuItem...) -> [AnyMenuItem] {
--- Sources/DSFMenuBuilder/MenuTitle.swift
+++ Sources/DSFMenuBuilder/MenuTitle.swift
@@ -1,8 +1,28 @@
 //
-//  File.swift
-//  
+//  MenuTitle.swift
 //
 //  Created by Darren Ford on 6/4/2022.
+//  Copyright © 2022 Darren Ford. All rights reserved.
+//
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
 import AppKit
@@ -12,11 +32,14 @@ import Foundation
 public struct MenuTitle {
 	public let title: String?
 	public let attributedTitle: NSAttributedString?
+
+	/// Create a title object using a String
 	public init(_ title: String) {
 		self.title = title
 		self.attributedTitle = nil
 	}
 
+	/// Create a title object with an attributed string for the title
 	public init(_ attributedTitle: NSAttributedString) {
 		self.title = nil
 		self.attributedTitle = attributedTitle
